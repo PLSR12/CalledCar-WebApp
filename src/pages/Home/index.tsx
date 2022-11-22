c330902970452d3d558c301a205bb3530bddd821
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-11-21 21:10:02 -0300

    [*] updated home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,25 @@
+import { TextAboutUs } from 'common/resources/home'
+import * as Molecules from 'components/Molecules'
+import * as S from './styles'
+
 export function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <S.Container>
+        <Molecules.Header />
+        <Molecules.Banner />
+        <S.AboutUs>
+          <h1>Sobre Nós</h1>
+          <p>{TextAboutUs}</p>
+        </S.AboutUs>
+        <S.MakeTrip>
+          <form>
+            <div className="div-form">
+              <p>teste </p>
+            </div>
+          </form>
+        </S.MakeTrip>
+      </S.Container>
+    </>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,1 +1,40 @@
 import styled from 'styled-components'
+
+export const Container = styled.section`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`
+
+export const AboutUs = styled.main`
+  display: flex;
+  width: 100%;
+  background-color: #ffffff;
+  flex-flow: column wrap;
+  padding-top: 40px;
+  margin: 0 auto;
+  h1 {
+    color: #000000;
+    font-size: 40px;
+    text-align: center;
+  }
+  p {
+    font-size: 20px;
+    color: #000000;
+    text-align: justify;
+    padding: 20px 50px 50px 50px;
+  }
+`
+export const MakeTrip = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  .div-form {
+    background: #eaeaea;
+    width: 500px;
+    height: 500px;
+    border-radius: 25px;
+    box-shadow: 5px 5px 5px 5px rgba(0, 0, 0, 0.575);
+  }
+`
