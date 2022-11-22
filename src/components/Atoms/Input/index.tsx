1dc1effccef66c91498f3f100062791b62470a42
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-11-21 21:58:29 -0300

    [*] fixed error eslint

--- src/components/Atoms/Input/index.tsx
+++ src/components/Atoms/Input/index.tsx
@@ -2,20 +2,15 @@ import React from 'react'
 import * as S from './styles'
 
 export const Input: React.FC<any> = React.forwardRef(
-  ({ type, name, id, placeholder, label, error, ...props }, ref) => {
+  ({ type = 'text', name, id = name, placeholder, label, error, ...props }, ref) => {
     return (
       <S.Container>
         <label htmlFor={name}>{label}</label>
-        <input
-          ref={ref}
-          {...props}
-          name={name}
-          id={id || name}
-          type={type || 'text'}
-          placeholder={placeholder}
-        />
+        <input ref={ref} {...props} name={name} id={id} type={type} placeholder={placeholder} />
         {!!error && <div>{error.message}</div>}
       </S.Container>
     )
   }
 )
+
+Input.displayName = 'InputComponent'
